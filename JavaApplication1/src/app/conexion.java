077c57018ddecee8935b60edb3b2cee5f9381a03
AbyssalDragon123 <alexander2004deleon@gmail.com>
2025-04-08 23:27:33 -0600

    DBALUMNOS

---
+++ JavaApplication1/src/app/conexion.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Admin
+ */
+public class conexion {
+    public static Connection getconexion(){
+        String url ="jdbc:sqlserver://localhost:1433"
+                + "database=escuela;"
+                + "user=sa;"
+               + "password=1234;";
+        try{
+            Connection con = DriverManager.getConnection(url);
+        return con;    
+        } catch(SQLException e){
+        System.out.println(e.toString());
+        return null;
+        }
+    }
+}
